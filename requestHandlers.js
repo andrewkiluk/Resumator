f92c9ce895edd6dddf63ddfc8a30a7ccf3ebee15
Andrew Kiluk <andrew.kiluk@gmail.com>
2014-05-30 19:12:56 -0400

    Basic functionality achieved!

--- requestHandlers.js
+++ requestHandlers.js
@@ -47,20 +47,123 @@ function resumate(request, response) {
 				req.connection.destroy();
 		});
 		request.on('end', function () {
-			console.log(body);
-
 			var fileId = murmur.hash128(body).hex();
+
+			var resumeData = JSON.parse(body);
+			console.log(resumeData);
 
 			// Here is where we create a TeX file.
 
-			var texString = "\\documentclass{article} \\begin{document} Let's try to make some TeX!\\end{document}";
+			var texString = "\\documentclass[" + resumeData.fontSize + "pt]{article}";
 
-			console.log("About to run LaTeX on the string: " + texString);
+			fs.readFile('header.tex', 'utf8', function (err,data) {
+				if (err) {
+					return console.log(err);
+				}
+				texString += data;
 
-			// Add a thing to encode any " characters as something else.
-			if(!fs.existsSync("directory")){
-				fs.mkdirSync("directory", 0766, function(err){
-					if(err) {
+			// texString now contains the header.
+
+			// Next we clean the data to avoid common LaTeX errors
+
+				if(resumeData.name === ""){
+					resumeData.name = "Name";
+				}
+
+
+			// Now we add the data to the texString
+
+				texString += "\\makeheading{" + resumeData.name + "}";
+				for(blockIndex = 0; blockIndex < resumeData.numBlocks; blockIndex++){
+
+				}
+
+				for(blockIndex = 0; blockIndex < resumeData.numBlocks; blockIndex++){
+					
+					blockLength = resumeData.blocks[blockIndex].numEntries;
+
+					texString += "\\section{" + resumeData.blocks[blockIndex].title + "}";
+
+					switch(resumeData.blocks[blockIndex].type){
+						case 'keyValueBlock':
+							texString += "\\newlength{\\rcollength}\\setlength{\\rcollength}{1.85in}";
+							texString += "\\begin{tabular}[t]{@{}p{\\textwidth-\\rcollength}p{\\rcollength}}"
+							for(entryIndex=0; entryIndex < blockLength; entryIndex++){
+								texString += "\\textit{" + resumeData.blocks[blockIndex].entries[entryIndex].key + "}: " + resumeData.blocks[blockIndex].entries[entryIndex].value +"\\\\\n ";
+							}
+							texString += "\\end{tabular}\n\n";
+							break;
+
+						case 'lr-keyValueBlock':
+							for(entryIndex=0; entryIndex < blockLength; entryIndex++){
+								texString += resumeData.blocks[blockIndex].entries[entryIndex].key + " \\hfill \\textbf{ " + resumeData.blocks[blockIndex].entries[entryIndex].value + "}\\\\\n\n "
+							}
+							break;
+
+						case 'descriptionBlock':
+							for(entryIndex=0; entryIndex < blockLength; entryIndex++){
+								texString += "\\emph{" + resumeData.blocks[blockIndex].entries[entryIndex].key + "}\\\\\n ";
+								texString += resumeData.blocks[blockIndex].entries[entryIndex].value + "\n ";
+
+								if(entryIndex < blockLength){
+									texString += "\n\\vspace{2mm}\n\n";
+								}
+							}
+
+							break;
+
+						case 'listBlock':
+
+							texString += "\\vspace{-8mm}\\begin{itemize}";
+
+							for(entryIndex=0; entryIndex < blockLength; entryIndex++){
+								texString += "\\item " + resumeData.blocks[blockIndex].entries[entryIndex].value;
+							}
+							texString += "\\end{itemize}\n\n";
+							break;
+
+						default:
+							console.log("switch failure on block type check");
+							break;
+					}
+				}
+
+
+
+				texString += "\\end{document}";
+
+
+
+				// Finished parsing the JSON and making the string, we're now ready to typeset.
+
+				console.log("About to run LaTeX.");
+
+				if(!fs.existsSync("temp")){
+					fs.mkdirSync("temp", 0766, function(err){
+						if(err) {
+							console.log(err);
+							response.writeHead(500, {"Content-Type": "text/plain"});
+							response.write(err + "\n");
+							response.end();
+							return;
+						}
+					});
+				}
+				fs.mkdir("temp/" + fileId, function(err){
+					if(!err || (err && err.code === 'EEXIST')){
+						fs.writeFile('temp/' + fileId + '/resume.tex', texString, function(err) {
+							if(err) {
+								console.log(err);
+							}
+							exec("pdflatex" + " -output-directory=temp/" + fileId
+							 + ' temp/' + fileId + '/resume.tex', function(error, stdout, stderr) {
+									console.log(stdout);
+							 		console.log(stderr);
+									response.end(fileId);
+							});
+						});
+					}
+					else{
 						console.log(err);
 						response.writeHead(500, {"Content-Type": "text/plain"});
 						response.write(err + "\n");
@@ -68,45 +171,9 @@ function resumate(request, response) {
 						return;
 					}
 				});
-			}
-			fs.mkdir("temp/" + fileId, function(err){
-				if(!err || (err && err.code === 'EEXIST')){
-					fs.writeFile('temp/' + fileId + '/resume.tex', texString, function(err) {
-						if(err) {
-							console.log(err);
-						}
-						exec("pdflatex" + " -aux_directory=temp/" + fileId + " -output-directory=temp/" + fileId
-						 + ' temp/' + fileId + '/resume.tex', function(error, stdout, stderr) {
-								console.log(stdout);
-						 		console.log(stderr);
-								response.end(fileId);
-						});
-					});
-				}
-				else{
-					console.log(err);
-					response.writeHead(500, {"Content-Type": "text/plain"});
-					response.write(err + "\n");
-					response.end();
-					return;
-				}
 			});
 		});
 	}
-
-		// We should write the entire data? No, probably just a link to it. But then we have to handle that. 
-		// Let's try writing the data of the file, then deleting the file from the server.
-
-		// A PHP example:
-		// header('Content-Type: application/pdf');
-		// header('Content-Disposition: attachment; filename=' . $file . ';');
-		// header('Content-Length: ' . filesize($folder . '/' . $file));
-
-		//readfile($folder . '/' . $file);
-	// response.write("Resumation!!!");
-
-
-	// response.end();
 }
 
 exports.serve = serve;
--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 var http = require("http");
 var url = require("url");
+var fs = require("fs");
 
 
 function start(route, handle) {
-  var onRequest = function(request, response) {
-    var pathname = url.parse(request.url).pathname;
-    console.log("Request for " + pathname + " received.");
+	var onRequest = function(request, response) {
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for " + pathname + " received.");
 
-    route(handle, pathname, request, response);
-    
-  }
-  http.createServer(onRequest).listen(8888);
-  console.log("The server has started.");
+		route(handle, pathname, request, response);
+		
+	}
+	http.createServer(onRequest).listen(8888);
+	console.log("The server has started.");
+
+	// The below will be a function which is called periodically to clean up old data.
+
+	// (function()
+	// {
+	// 	setInterval(function() {
+	// 		fs.rmdir(path, callback)
+	// 	}, 5*60*1000 );
+	// })();
 }
 
 exports.start = start;
